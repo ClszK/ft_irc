82dddcb37beaf8a82b3af3510154bbe0be0e7db4
Clsz <45088680+ClszK@users.noreply.github.com>
2024-09-22 03:09:22 +0900

    ft_irc: merge

--- include/command/ModeCommand.hpp
+++ include/command/ModeCommand.hpp
@@ -7,9 +7,4 @@
 class ModeCommand : public ICommand {
  public:
   virtual std::string execute(Client& client, Message& message);
-
- private:
-  static const std::set<char> allowedMode;
-
-  static const std::set<char> createAllowedModeSet();
 };
--- src/command/ModeCommand.cpp
+++ src/command/ModeCommand.cpp
@@ -1,19 +1,5 @@
 #include "command/ModeCommand.hpp"
 
-const std::set<char> ModeCommand::createAllowedModeSet() {
-  std::set<char> tempSet;
-
-  tempSet.insert('i');
-  tempSet.insert('t');
-  tempSet.insert('k');
-  tempSet.insert('o');
-  tempSet.insert('l');
-  return tempSet;
-}
-
-const std::set<char> ModeCommand::allowedMode =
-    ModeCommand::createAllowedModeSet();
-
 /**
  * - i : 초대 전용 채널 설정 / 제거
  * - t : 채널 운영자에 대한 TOPIC 명령 제한 설정 / 제거
@@ -36,50 +22,52 @@ std::string ModeCommand::execute(Client& client, Message& message) {
   if (message.params.size() == 1)
     return ReplyUtility::makeChannelModeIsReply(client, *channel);
 
-  size_t paramIdx = 2, addMode;
+  // size_t paramIdx = 2, addMode;
 
-  for (size_t i = 0; i < channelSetMode.size(); i++) {
-    if (channelSetMode[i] == '+')
-      addMode = 1;
-    else if (channelSetMode[i] == '-')
-      addMode = 0;
+  // for (size_t i = 0; i < channelSetMode.size(); i++) {
+  //   if (channelSetMode[i] == '+')
+  //     addMode = 1;
+  //   else if (channelSetMode[i] == '-')
+  //     addMode = 0;
 
-    char mode = channelSetMode[i];
+  //   char mode = channelSetMode[i];
 
-    switch (mode) {
-      case 'i':
-      case 't':
-        if (addMode)
-          channel->setChannelModeAdd(mode);
-        else
-          channel->setChannelModeSub(mode);
-        break;
-      case 'k':
-        if (addMode) {
-          if (message.params.size() <= paramIdx)
-            return ReplyUtility::makeErrNotExistReply(client, channelName,
-                                                      mode);
-          channel->setChannelKey(message.params[paramIdx++]);
-          channel->setChannelModeAdd(mode);
-        } else {
-          channel->setChannelKey("");
-          channel->setChannelModeSub(mode);
-        }
-        break;
-      case 'o':
-        if (message.params.size() <= paramIdx)
-          return ReplyUtility::makeErrNotExistReply(client, channelName, mode);
-        std::string nickName = message.params[paramIdx++];
-        Client* target = Client::findClient(nickName);
+  //   switch (mode) {
+  //     case 'i':
+  //     case 't':
+  //       if (addMode)
+  //         channel->setChannelModeAdd(mode);
+  //       else
+  //         channel->setChannelModeSub(mode);
+  //       break;
+  //     case 'k':
+  //       if (addMode) {
+  //         if (message.params.size() <= paramIdx)
+  //           return ReplyUtility::makeErrNotExistReply(client, channelName,
+  //                                                     mode);
+  //         channel->setChannelKey(message.params[paramIdx++]);
+  //         channel->setChannelModeAdd(mode);
+  //       } else {
+  //         channel->setChannelKey("");
+  //         channel->setChannelModeSub(mode);
+  //       }
+  //       break;
+  //     case 'o':
+  //       if (message.params.size() <= paramIdx)
+  //         return ReplyUtility::makeErrNotExistReply(client, channelName,
+  //         mode);
+  //       std::string nickName = message.params[paramIdx++];
+  //       Client* target = Client::findClient(nickName);
 
-        if (addMode) {
-          if (target == NULL)
-            return ReplyUtility::makeErrNoSuchNickReply(client, nickName);
-          channel->setGMListAdd(*target);
-        } else
-          channel->setGMListSub(*target);
+  //       if (addMode) {
+  //         if (target == NULL)
+  //           return ReplyUtility::makeErrNoSuchNickReply(client, nickName);
+  //         channel->setGMListAdd(*target);
+  //       } else
+  //         channel->setGMListSub(*target);
 
-        break;
-    }
-  }
+  //       break;
+  //   }
+  // }
+  return "";
 }
